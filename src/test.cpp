36cafa03e92de8960bb3f57de681413e7da534c3
Minh Vu <minhvh99@gmail.com>
2026-07-17 01:12:12 +0700

    ci: print test results to gha summary (#2)

--- src/test.cpp
+++ src/test.cpp
@@ -1,5 +1,23 @@
 #include "test.h"
 
+#include <cstring>
+#include <fstream>
+
 int main() {
-    return test::run_tests();
+    std::ofstream gha_summary("/dev/null");
+    if (!gha_summary.is_open()) {
+        LOG_ERROR("cannot open /dev/null");
+        return 4;
+    }
+    const char* gh_actions_env = std::getenv("GITHUB_ACTIONS");
+    bool is_gha = gh_actions_env != nullptr && strcmp(gh_actions_env, "true") == 0;
+    const char* gh_summary_env = std::getenv("GITHUB_STEP_SUMMARY");
+    if (is_gha && gh_summary_env != nullptr) {
+        gha_summary = std::ofstream(gh_summary_env, std::ios::app);
+        if (!gha_summary.is_open()) {
+            LOG_ERROR("cannot open {}", gh_summary_env);
+            return 4;
+        }
+    }
+    return test::run_tests(gha_summary);
 }
